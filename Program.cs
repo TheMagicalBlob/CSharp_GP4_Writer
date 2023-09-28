5fb1acf198f8a1111df8304a62861e2561b327ef
TheMagicalBlob <alteringorbisgames@gmail.com>
2023-09-28 06:54:36 -0300

    0.2.1

--- Program.cs
+++ Program.cs
@@ -16,36 +16,63 @@ namespace CSGP4POC {
 
             // Main Variables
             byte chunk_count, scenario_count, default_id;
-            string content_id, passcode, storage_type, app_type;
+            byte[] DataBuffer;
+            string content_id, passcode = "00000000000000000000000000000000", storage_type, app_type;
+            string[] ChunkLabelArray;
             var TimeStamp = $"{DateTime.Now.GetDateTimeFormats()[78]}";
 
             // Base Xml And XmlDeclaration
             var GP4 = new XmlDocument();
             var Declaration = GP4.CreateXmlDeclaration("1.1", "utf-8", "yes");
 
-            void readPlaygoChunks() {
-                using(var playgo_chunks_dat = File.OpenRead($@"{args[0]}\sce_sys\playgo-chunk.dat")) {
-                    playgo_chunks_dat.Position = 0x0A;
-                    chunk_count = (byte)playgo_chunks_dat.ReadByte();
+            StringBuilder ReadLabel() {
 
-                    playgo_chunks_dat.Position = 0x0E;
-                    scenario_count = (byte)playgo_chunks_dat.ReadByte();
-                
-                    playgo_chunks_dat.Position = 0x14;
-                    default_id = (byte)playgo_chunks_dat.ReadByte();
 
-                    playgo_chunks_dat.Position = 0x40;
-                    var ContentIdString = new byte[36];
-                    playgo_chunks_dat.Read(ContentIdString, 0, 36);
-                    content_id = Encoding.UTF8.GetString(ContentIdString);
+                return new String();
+            }
+
+            // Read playgo-chunks.dat And Param.sfo To Get Most Variables
+            using(var playgo_chunks_dat = File.OpenRead($@"{args[0]}\sce_sys\playgo-chunk.dat")) {
+                playgo_chunks_dat.Position = 0x0A;
+                chunk_count = (byte)playgo_chunks_dat.ReadByte();
+                ChunkLabelArray = new string[chunk_count];
+
+                playgo_chunks_dat.Position = 0x0E;
+                scenario_count = (byte)playgo_chunks_dat.ReadByte();
+
+                playgo_chunks_dat.Position = 0x14;
+                default_id = (byte)playgo_chunks_dat.ReadByte();
+
+                DataBuffer = new byte[36];
+                playgo_chunks_dat.Position = 0x40;
+                playgo_chunks_dat.Read(DataBuffer, 0, 36);
+                content_id = Encoding.UTF8.GetString(DataBuffer);
+
+
+                // Get Length Of Chunk Label String Array
+                DataBuffer = new byte[4];
+                playgo_chunks_dat.Position = 0xD0;
+                playgo_chunks_dat.Read(DataBuffer, 0, 4);
+                var ChunkLabelPointer = BitConverter.ToInt32(DataBuffer, 0);
+                DataBuffer = new byte[4];
+                playgo_chunks_dat.Position = 0xD8;
+                playgo_chunks_dat.Read(DataBuffer, 0, 4);
+                var EndOfChunkLabels = BitConverter.ToInt32(DataBuffer, 0);
+
+                // Load Chunk Labels
+                DataBuffer = new byte[EndOfChunkLabels - ChunkLabelPointer];
+                playgo_chunks_dat.Position = ChunkLabelPointer;
+                for(int i = 0; i < chunk_count; i++) {
+                    playgo_chunks_dat.Read(DataBuffer, 0, DataBuffer.Length);
+                    Console.WriteLine($"{DataBuffer.Length} {Encoding.UTF8.GetString(DataBuffer)}");
+                    Console.Read();
                 }
+            }
+            using(var param_sfo = File.OpenRead($@"{args[0]}\sce_sys\param.sfo")) {
 
             }
-            void readParamFile() {
-                var param_sfo = File.OpenRead($@"{args[0]}\sce_sys\param.sfo");
-            }
 
-            readPlaygoChunks();
+
 
 
             var RootNode = GP4.CreateElement("psproject");
@@ -53,7 +80,6 @@ namespace CSGP4POC {
             RootNode.SetAttribute("version", "1000");
 
             var volume = GP4.CreateElement("volume");
-
             var volume_type = GP4.CreateElement("volume_type");
             volume_type.InnerText = "ps4_pkg_app";
             var volume_id = GP4.CreateElement("volume_id");
@@ -63,7 +89,7 @@ namespace CSGP4POC {
 
             var package = GP4.CreateElement("package");
             package.SetAttribute("content_id", content_id);
-            package.SetAttribute("passcode", "00000000000000000000000000000000");
+            package.SetAttribute("passcode", passcode);
             package.SetAttribute("storage_type", "(storage_type Here)");
             package.SetAttribute("app_type", "(app_type Here)");
 
@@ -72,21 +98,24 @@ namespace CSGP4POC {
             chunk_info.SetAttribute("scenario_count", $"{scenario_count}");
 
             var chunks = GP4.CreateElement("chunks");
+            var chunk = GP4.CreateElement("chunk");
 
-
-
+            // Build .gp4 Structure
             GP4.AppendChild(Declaration);
             GP4.AppendChild(RootNode);
             RootNode.AppendChild(volume);
-
             volume.AppendChild(volume_type);
             volume.AppendChild(volume_id);
             volume.AppendChild(volume_ts);
             volume.AppendChild(package);
             volume.AppendChild(chunk_info);
             chunk_info.AppendChild(chunks);
-
-
+            for(int chunk_id = 0; chunk_id < chunk_count; chunk_id++) {
+                chunk.SetAttribute("id", $"{chunk_id}");
+                chunk.SetAttribute("label", $"{chunk_id}");
+                chunks.AppendChild(chunk);
+            }
+            // gee, I wonder what this does
             GP4.Save(@"C:\Users\Blob\Desktop\CUSA00557-Test.gp4");
         }
     }
