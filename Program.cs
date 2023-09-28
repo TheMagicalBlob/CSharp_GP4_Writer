06613a9970a9af9de93a32867a58490e1969df77
TheMagicalBlob <alteringorbisgames@gmail.com>
2023-09-28 17:43:01 -0300

    ver 0.6 - chunk_info finished

--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ namespace CSGP4POC {
          0xD8: chunk label end (Padded)
          0xE0: Senario 1 type (*0xE0 + 0x20 For Each Scenario After?)
          0xF0: Scenario Labels
+         0xF4: Scenario Label Array Byte Length
          
         param.sfo
          0x00 - 0x8: Head
@@ -41,16 +42,16 @@ namespace CSGP4POC {
         */
 
         // Pass Game Root As Arg
-        static void Main(string[] args) { // ver 0.5.1
+        static void Main(string[] args) { // ver 0.6
             string[] RequiredVariables = new string[] { "APP_VER", "CATEGORY", "CONTENT_ID", "TITLE_ID", "VERSION" };
 
 
             // Main Variables
-            byte[] DataBuffer;
+            byte[] BufferArray;
             int chunk_count, scenario_count, default_id;
-            int[] scenario_types, initial_chunk_count, scenario_chunk_range;
+            int[] scenario_types, scenario_chunk_range, initial_chunk_count;
             string app_ver, version, content_id, title_id = "CUSA12345", passcode = "00000000000000000000000000000000", category = "?";
-            string[] ChunkLabelArray, ParamNameArray;
+            string[] chunk_labels, parameter_labels, scenario_labels;
             var TimeStamp = $"{DateTime.Now.GetDateTimeFormats()[78]}";
 
             // Base Xml And XmlDeclaration
@@ -65,8 +66,8 @@ namespace CSGP4POC {
                 for(int stringIndex = 0; stringIndex < StringArray.Length; stringIndex++) {
                     Builder = new StringBuilder();
 
-                    while(DataBuffer[byteIndex] != 0)
-                        Builder.Append(Encoding.UTF8.GetString(new byte[] { DataBuffer[byteIndex++] })); // Just Take A Byte, You Fussy Prick
+                    while(BufferArray[byteIndex] != 0)
+                        Builder.Append(Encoding.UTF8.GetString(new byte[] { BufferArray[byteIndex++] })); // Just Take A Byte, You Fussy Prick
 
                     byteIndex++;
                     StringArray[stringIndex] = Builder.ToString();
@@ -79,12 +80,13 @@ namespace CSGP4POC {
                 // Read Chunk Count
                 playgo.Position = 0x0A;
                 chunk_count = (byte)playgo.ReadByte();
-                ChunkLabelArray = new string[chunk_count];
+                chunk_labels = new string[chunk_count];
 
                 // Read Scenario Count
                 playgo.Position = 0x0E;
                 scenario_count = (byte)playgo.ReadByte();
                 scenario_types = new int[scenario_count];
+                scenario_labels = new string[scenario_count];
                 initial_chunk_count = new int[scenario_count];
                 scenario_chunk_range = new int[scenario_count];
 
@@ -93,118 +95,144 @@ namespace CSGP4POC {
                 default_id = (byte)playgo.ReadByte();
 
                 // Read Content ID Here Instead Of The .sfo Because Meh, User Has Bigger Issues If Those Aren't the Same
-                DataBuffer = new byte[36];
+                BufferArray = new byte[36];
                 playgo.Position = 0x40;
-                playgo.Read(DataBuffer, 0, 36);
-                content_id = Encoding.UTF8.GetString(DataBuffer);
+                playgo.Read(BufferArray, 0, 36);
+                content_id = Encoding.UTF8.GetString(BufferArray);
 
-                // Get Chunk Label Start Address From Pointer
-                DataBuffer = new byte[4];
+                // Read Chunk Label Start Address From Pointer
+                BufferArray = new byte[4];
                 playgo.Position = 0xD0;
-                playgo.Read(DataBuffer, 0, 4);
-                var ChunkLabelPointer = BitConverter.ToInt32(DataBuffer, 0);
+                playgo.Read(BufferArray, 0, 4);
+                var chunk_label_pointer = BitConverter.ToInt32(BufferArray, 0);
 
-                // Get Length Of Chunk Label Byte Array
+                // Read Length Of Chunk Label Byte Array
                 playgo.Position = 0xD4;
-                playgo.Read(DataBuffer, 0, 4);
-                var ChunkLabelArrayLength = BitConverter.ToInt32(DataBuffer, 0);
+                playgo.Read(BufferArray, 0, 4);
+                var chunk_label_array_length = BitConverter.ToInt32(BufferArray, 0);
 
                 // Load Scenario(s)
                 playgo.Position = 0xE0;
-                playgo.Read(DataBuffer, 0, 4);
-                var scenarioPointer = BitConverter.ToInt32(DataBuffer, 0);
-                for(int scenario_index = 0; scenario_index < scenario_count - 1; scenario_index++) {
-                    // Get Scenario Type
+                playgo.Read(BufferArray, 0, 4);
+                var scenarioPointer = BitConverter.ToInt32(BufferArray, 0);
+                for(int scenario_index = 0; scenario_index < scenario_count; scenario_index++) {
+                    // Read Scenario Type
                     playgo.Position = scenarioPointer;
                     scenario_types[scenario_index] = (byte)playgo.ReadByte();
 
-                    // Get Scenario initial_chunk_count
+                    // Read Scenario initial_chunk_count
                     playgo.Position = (scenarioPointer + 0x14);
-                    playgo.Read(DataBuffer, 2, 2);
-                    initial_chunk_count[scenario_index] = BitConverter.ToInt16(DataBuffer, 2);
-                    playgo.Read(DataBuffer, 2, 2);
-                    scenario_chunk_range[scenario_index] = BitConverter.ToInt16(DataBuffer, 2);
+                    playgo.Read(BufferArray, 2, 2);
+                    initial_chunk_count[scenario_index] = BitConverter.ToInt16(BufferArray, 2);
+                    playgo.Read(BufferArray, 2, 2);
+                    scenario_chunk_range[scenario_index] = BitConverter.ToInt16(BufferArray, 2);
                     scenarioPointer += 0x20;
                 }
 
+                // Load Scenario Label Array Byte Length
+                BufferArray = new byte[2];
+                playgo.Position = 0xF4;
+                playgo.Read(BufferArray, 0, 2);
+                var scenario_label_array_length = BitConverter.ToInt16(BufferArray, 0);
+
+                // Load Scenario Label Pointer
+                playgo.Position = 0xF0;
+                BufferArray = new byte[4];
+                playgo.Read(BufferArray, 0, 4);
+                var scenario_label_array_pointer = BitConverter.ToInt32(BufferArray, 0);
+
+                // Load Scenario Labels
+                playgo.Position = scenario_label_array_pointer;
+                BufferArray = new byte[scenario_label_array_length];
+                playgo.Read(BufferArray, 0, BufferArray.Length);
+                LoadParameterLabels(scenario_labels);
+
                 // Load Chunk Labels
-                DataBuffer = new byte[ChunkLabelArrayLength];
-                playgo.Position = ChunkLabelPointer;
-                playgo.Read(DataBuffer, 0, DataBuffer.Length);
-                LoadParameterLabels(ChunkLabelArray);
+                BufferArray = new byte[chunk_label_array_length];
+                playgo.Position = chunk_label_pointer;
+                playgo.Read(BufferArray, 0, BufferArray.Length);
+                LoadParameterLabels(chunk_labels);
             }
 
-            // Get Length Of Chunk Label String Array
+
+            ////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+            ///--    Parse param.sfo For Various Parameters    --\\\
+            ////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\
             using(var sfo = File.OpenRead($@"{args[0]}\sce_sys\param.sfo")) {
+                // Read Pointer For Array Of Parameter Names
                 sfo.Position = 0x8;
-                DataBuffer = new byte[4];
-                sfo.Read(DataBuffer, 0, 4);
-                var ParamNameArrayPointer = BitConverter.ToInt32(DataBuffer, 0);
+                BufferArray = new byte[4];
+                sfo.Read(BufferArray, 0, 4);
+                var ParamNameArrayPointer = BitConverter.ToInt32(BufferArray, 0);
 
+                // Read Base Pointer For .pkg Parameters
                 sfo.Position = 0x0C;
-                sfo.Read(DataBuffer, 0, 4);
-                var ParamVariablesPointer = BitConverter.ToInt32(DataBuffer, 0);
+                sfo.Read(BufferArray, 0, 4);
+                var ParamVariablesPointer = BitConverter.ToInt32(BufferArray, 0);
 
+                // Read Parameter Name Array Length And Initialize Offset Array
                 sfo.Position = 0x10;
-                sfo.Read(DataBuffer, 0, 4);
-                var ParamNameArrayLength = BitConverter.ToInt32(DataBuffer, 0);
-
+                sfo.Read(BufferArray, 0, 4);
+                var ParamNameArrayLength = BitConverter.ToInt32(BufferArray, 0);
                 int[] ParameterOffsets = new int[ParamNameArrayLength];
 
                 // Load Parameter Names
-                DataBuffer = new byte[ParamVariablesPointer - ParamNameArrayPointer];
-                ParamNameArray = new string[ParamNameArrayLength];
+                BufferArray = new byte[ParamVariablesPointer - ParamNameArrayPointer];
+                parameter_labels = new string[ParamNameArrayLength];
                 sfo.Position = ParamNameArrayPointer;
-                sfo.Read(DataBuffer, 0, DataBuffer.Length);
-                LoadParameterLabels(ParamNameArray);
+                sfo.Read(BufferArray, 0, BufferArray.Length);
+                LoadParameterLabels(parameter_labels);
 
                 // Load Parameter Offsets
                 sfo.Position = 0x20;
-                DataBuffer = new byte[4];
-                for(int offsetIndex = 0; offsetIndex < ParamNameArrayLength ; sfo.Position += (0x10 - DataBuffer.Length)) {
-                    sfo.Read(DataBuffer, 0, 4);
-                    ParameterOffsets[offsetIndex] = ParamVariablesPointer + BitConverter.ToInt32(DataBuffer, 0);
+                BufferArray = new byte[4];
+                for(int offsetIndex = 0; offsetIndex < ParamNameArrayLength ; sfo.Position += (0x10 - BufferArray.Length)) {
+                    sfo.Read(BufferArray, 0, 4);
+                    ParameterOffsets[offsetIndex] = ParamVariablesPointer + BitConverter.ToInt32(BufferArray, 0);
                     offsetIndex++;
                 }
 
-                // Load The Rest Of The .sfo Variables
+                // Load The Rest Of The Required .pkg Variables From param.sfo
                 for(int Index = 0; Index < ParamNameArrayLength; Index++)
-                    if(RequiredVariables.Contains(ParamNameArray[Index])) {
-                        sfo.Position = ParameterOffsets[Index];
-                        DataBuffer = new byte[4];
+                    if(RequiredVariables.Contains(parameter_labels[Index])) { // Ignore Variables Not Needed For .gp4 Project Creation
 
-                        switch(ParamNameArray[Index]) { // I'm Too Tired to think of a more elegant solution right now. If it works, it works
+                        sfo.Position = ParameterOffsets[Index];
+                        BufferArray = new byte[4];
+
+                        switch(parameter_labels[Index]) { // I'm Too Tired to think of a more elegant solution right now. If it works, it works
 
                             case "APP_VER":
-                                DataBuffer = new byte[5];
-                                sfo.Read(DataBuffer, 0, 5);
-                                app_ver = Encoding.UTF8.GetString(DataBuffer);
+                                BufferArray = new byte[5];
+                                sfo.Read(BufferArray, 0, 5);
+                                app_ver = Encoding.UTF8.GetString(BufferArray);
                                 break;
-                            case "CATEGORY":
-                                sfo.Read(DataBuffer, 0, 2);
-                                category = Encoding.UTF8.GetString(DataBuffer, 0, 2);
+                            case "CATEGORY": // gd / gp
+                                sfo.Read(BufferArray, 0, 2);
+                                category = Encoding.UTF8.GetString(BufferArray, 0, 2);
                                 break;
                             case "CONTENT_ID":
-                                DataBuffer = new byte[36];
-                                sfo.Read(DataBuffer, 0, 36);
-                                content_id = Encoding.UTF8.GetString(DataBuffer);
+                                BufferArray = new byte[36];
+                                sfo.Read(BufferArray, 0, 36);
+                                content_id = Encoding.UTF8.GetString(BufferArray);
                                 break;
                             case "TITLE_ID":
-                                DataBuffer = new byte[9];
-                                sfo.Read(DataBuffer, 0, 9);
-                                title_id = Encoding.UTF8.GetString(DataBuffer);
+                                BufferArray = new byte[9];
+                                sfo.Read(BufferArray, 0, 9);
+                                title_id = Encoding.UTF8.GetString(BufferArray);
                                 break;
-                            case "VERSION":
-                                DataBuffer = new byte[5];
-                                sfo.Read(DataBuffer, 0, 5);
-                                version = Encoding.UTF8.GetString(DataBuffer);
+                            case "VERSION": // Remaster
+                                BufferArray = new byte[5];
+                                sfo.Read(BufferArray, 0, 5);
+                                version = Encoding.UTF8.GetString(BufferArray);
                                 break;
                         }
                     }
             }
 
 
-            // Create Base .gp4 Elements
+            ///////////////////////\\\\\\\\\\\\\\\\\\\\\\
+            ///--     Create Base .gp4 Elements     --\\\
+            ///////////////////////\\\\\\\\\\\\\\\\\\\\\\
             var RootNode = GP4.CreateElement("psproject");
                 RootNode.SetAttribute("fmt", "GP4");
                 RootNode.SetAttribute("version", "1000");
@@ -235,18 +263,21 @@ namespace CSGP4POC {
             var scenarios = GP4.CreateElement("scenarios");
                 scenarios.SetAttribute("default_id", $"{default_id}");
 
-            for(int id = 0; id < scenario_count - 1; id++) {
+            for(int id = 0; id < scenario_count; id++) {
                 scenario = GP4.CreateElement("scenario");
                 scenario.SetAttribute("id", $"{id}");
-                scenario.SetAttribute("type", $"{id}");
+                scenario.SetAttribute("type", $"{(scenario_types[id] == 1 ? "sp" : "mp")}");
                 scenario.SetAttribute("initial_chunk_count", $"{initial_chunk_count[id]}");
-                scenario.SetAttribute("label", $"{id}");
-                scenario.InnerText = $"0-{scenario_chunk_range[id]}";
+                scenario.SetAttribute("label", $"{scenario_labels[id]}");
+                scenario.InnerText = $"0-{scenario_chunk_range[id] - 1}";
+                scenarios.AppendChild(scenario);
             }
 
 
 
-            // Build .gp4 Structure
+            ////////////////////\\\\\\\\\\\\\\\\\\\\
+            ///--     Build .gp4 Structure     --\\\
+            ////////////////////\\\\\\\\\\\\\\\\\\\\
             GP4.AppendChild(Declaration);
             GP4.AppendChild(RootNode);
             RootNode.AppendChild(volume);
@@ -259,10 +290,11 @@ namespace CSGP4POC {
             for(int chunk_id = 0; chunk_id < chunk_count; chunk_id++) {
                 chunk = GP4.CreateElement("chunk");
                 chunk.SetAttribute("id", $"{chunk_id}");
-                chunk.SetAttribute("label", $"{ChunkLabelArray[chunk_id]}");
+                chunk.SetAttribute("label", $"{chunk_labels[chunk_id]}");
 
                 chunks.AppendChild(chunk);
             }
+            chunk_info.AppendChild(scenarios);
 
             // gee, I wonder what this does
             GP4.Save($@"C:\Users\Blob\Desktop\_{title_id}-{(category == "gd" ? "app" : "patch")}.gp4");
